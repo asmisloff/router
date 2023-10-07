f4f3724bb80b741c6431d64f372cb4082c9297fe
asmisloff <a.smisloff@gmail.com>
2023-10-07 16:44:22 +0300

    сечение цепочки ТС

--- prepareNodes.py
+++ prepareNodes.py
@@ -1,13 +1,15 @@
 from collections import deque
+from dataclasses import dataclass
 from functools import total_ordering
 import heapq
-from typing import Callable, Dict, Iterable, List, Set, Tuple, TypeVar
+from typing import Dict, List, Set, Tuple
 import numpy as np
+
 
 @total_ordering
 class Node:
     def __init__(self, x: float, li: int, breaking: bool = False) -> None:
-        self.x: int = round(round(x, 3) * 1000) # до метра
+        self.x: int = round(round(x, 3) * 1000)  # до метра
         self.lineIndex: int = li
         self.breaking: bool = breaking
 
@@ -17,9 +19,9 @@ class Node:
     def branchIndex(self):
         return self.lineIndex // 10_000
 
-    def setAxisCoordinate(self, x_: float): # для обратной совместимости
+    def setAxisCoordinate(self, x_: float):  # для обратной совместимости
         self.x = round(round(x_, 3) * 1000)
-    
+
     def getAxisCoordinate(self) -> float:
         return round(self.x / 1000, 3)
 
@@ -31,7 +33,7 @@ class Node:
 
     def __repr__(self) -> str:
         return str(self.x)
-    
+
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, Node):
             return False
@@ -49,14 +51,14 @@ class Node:
 class NetworkSection:
     def __init__(self) -> None:
         self.nodes: List[Node] = []
-    
+
     @classmethod
     def fromDict(cls, nodes: List[Node]) -> "NetworkSection":
         s = NetworkSection()
         s.nodes = nodes[:]
-        s.nodes.sort(key = lambda n: n.lineIndex)
+        s.nodes.sort(key=lambda n: n.lineIndex)
         return s
-    
+
     def copy(self, networkChainLinkLines: Set[int], duplicateBreakinNodes: bool) -> "NetworkSection":
         c = NetworkSection()
         for n in self.nodes:
@@ -66,32 +68,73 @@ class NetworkSection:
                     c.nodes[-1] = n.copyToLineIndex(n.lineIndex)
         return c
 
-    def size(self):
-        return len(self.nodes)
-    
     def __repr__(self) -> str:
         return str([n.x for n in self.nodes])
 
 
+@dataclass
 class BranchNetworkChainLink:
-    def __init__(self, xRight: float, lines: Set[int]) -> None:
-        self.xRight: int = round(round(xRight, 3) * 1000)
-        self.lines = lines
+    xLeft: int
+    xRight: int
+    lines: Set[int]
 
 
 class BranchNetworkChain:
     def __init__(self, chainLinks: List[BranchNetworkChainLink]) -> None:
         self.chainLinks = sorted(chainLinks, key=lambda cl: cl.xRight)
-    
+        self.__idx = 0
+
+    @classmethod
+    def fromAcNetworkFullDto(cls, dto: List[Tuple[float, int]]) -> "BranchNetworkChain":
+        xLeft = np.iinfo(np.int32).min
+        chainLinks = []
+        for x, trackQty in dto:
+            xRight = round(round(x, 3) * 1000)
+            chainLinks.append(
+                BranchNetworkChainLink(
+                    xLeft, xRight, set(range(1, trackQty + 1)))
+            )
+            xLeft = xRight
+        return BranchNetworkChain(chainLinks)
+
     def findChainLink(self, x: int) -> BranchNetworkChainLink | None:
-        for cl in self.chainLinks:
-            if cl.xRight >= x:
-                return cl
-        return None
-    
+        idx = self.__findIndex(x)
+        if idx is None:
+            return None
+        return self.chainLinks[idx]
+
+    def getLines(self, x: int) -> Set[int] | None:
+        idx = self.__findIndex(x)
+        if idx is None:
+            return None
+        cl = self.chainLinks[idx]
+        if cl.xRight == x and idx != len(self.chainLinks) - 1:
+            clr = self.chainLinks[idx + 1]
+            return cl.lines.union(clr.lines)
+        return cl.lines
+
+    def __findIndex(self, x: int) -> int | None:
+        cl = self.chainLinks[self.__idx]
+        if x == cl.xLeft:
+            return self.__idx - 1
+        elif cl.xRight == x:
+            if self.__idx < len(self.chainLinks) - 1:
+                self.__idx += 1
+            return self.__idx
+        elif x > self.chainLinks[-1].xRight:
+            return None
+        elif x < cl.xLeft:
+            self.__idx -= 1
+            return self.__findIndex(x)
+        elif x > cl.xRight:
+            self.__idx += 1
+            return self.__findIndex(x)
+        else:  # x > cl.xLeft and x < cl.xRight
+            return self.__idx
+
     def __repr__(self) -> str:
         return "_".join(str(cl.xRight) for cl in self.chainLinks)
-        
+
 
 def arrangeNodesByBranchIndex(nodes: Set[Node]) -> Dict[int, Dict[int, List[Node]]]:
     res: Dict[int, Dict[int, List[Node]]] = {}
@@ -128,6 +171,7 @@ def addNetworkChainNodes(nodes: Dict[int, Dict[int, List[Node]]], network: List[
 
 def buildBranchPartitions(branchNodeQueues: Dict[int, List[Node]], branchNetwork: BranchNetworkChain):
     partitions: List[Tuple[NetworkSection, NetworkSection]] = []
+
     def appendPartition():
         ntw = branchNetwork.findChainLink(out[0].nodes[0].x)
         if ntw is None:
@@ -135,11 +179,11 @@ def buildBranchPartitions(branchNodeQueues: Dict[int, List[Node]], branchNetwork
         leftSection = out.popleft().copy(ntw.lines, True)
         rightSection = out[0].copy(ntw.lines, False)
         partitions.append((leftSection, rightSection))
-    
+
     allLineIndices: Set[int] = set()
     for ntw in branchNetwork.chainLinks:
         allLineIndices.update(ntw.lines)
-    tmp: Dict[int, Node | None] = {key : None for key in allLineIndices}
+    tmp: Dict[int, Node | None] = {key: None for key in allLineIndices}
     out: deque[NetworkSection] = deque()
 
     while (True):
@@ -164,7 +208,8 @@ def buildBranchPartitions(branchNodeQueues: Dict[int, List[Node]], branchNetwork
                 tmp[key] = node
         if len(out) == 2:
             appendPartition()
-        out.append(NetworkSection.fromDict([n for n in tmp.values() if n is not None]))
+        out.append(NetworkSection.fromDict(
+            [n for n in tmp.values() if n is not None]))
     appendPartition()
     return partitions
 
@@ -175,17 +220,14 @@ if __name__ == "__main__":
     network: List[BranchNetworkChain] = []
     for branchIndex in range(3):
         for i in range(1, 4):
-            nodes.update([Node(x, 10_000 * branchIndex + i) for x in rng.random(i + 1) * 100])
-        network.append(
-            BranchNetworkChain(
-                [BranchNetworkChainLink(x, set((1, 2))) for x in rng.random(2) * 100]
-            )
-        )
+            nodes.update([Node(x, 10_000 * branchIndex + i)
+                         for x in rng.random(i + 1) * 100])
+        network.append(BranchNetworkChain.fromAcNetworkFullDto([(x, 2) for x in rng.random(2) * 150]))
 
-    print(network)
+    print(f"network: {network}")
     nodeQueues = arrangeNodesByBranchIndex(nodes)
-    print(nodeQueues)
+    print(f"nodeQueues: {nodeQueues}")
     addNetworkChainNodes(nodeQueues, network)
-    
+
     partitions = buildBranchPartitions(nodeQueues[0], network[0])
     print(partitions)
